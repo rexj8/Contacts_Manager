59a6005db775efdd3aaef569cb00b2e9b03e7bc4
TheEternalShenron <rachit.gla_cs18@gla.ac.in>
2020-02-14 23:13:43 +0530

    created MD5 method in Encryption

--- src/definition/Encryption.java
+++ src/definition/Encryption.java
@@ -1,6 +1,34 @@
 package definition;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Encryption {
+
+    /*
+     * This method takes String password and convert it into it's hash form
+     *
+     * @param String password
+     * @return It returns encrypted form of password
+     */
+    public static String MD5(String pswrd) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(pswrd.getBytes());
+            BigInteger number = new BigInteger(1, messageDigest);
+            String hashtext = number.toString(16);
+
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException();
+        }
+
+    }
 
 
 }
