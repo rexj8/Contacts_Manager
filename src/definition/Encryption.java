ea9c18cb34b5b513c74e0a166914d7e0d2875dbc
TheEternalShenron <rachit.gla_cs18@gla.ac.in>
2020-02-14 23:14:59 +0530

    created SHA1 method in Encryption

--- src/definition/Encryption.java
+++ src/definition/Encryption.java
@@ -30,5 +30,23 @@ public class Encryption {
 
     }
 
+    public static String SHA1(String pswrd) {
+
+        try {
+
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            byte[] result = md.digest(pswrd.getBytes());
+            StringBuffer sb = new StringBuffer();
+
+            for (int i = 0; i < result.length; i++) {
+                sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            return sb.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException();
+        }
+    }
+
 
 }
