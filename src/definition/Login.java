2ba77d810a29d0a3fe01ce00efdfc15efcda3e76
TheEternalShenron <rachit.gla_cs18@gla.ac.in>
2020-02-14 23:44:32 +0530

    created checkID in login class

--- src/definition/Login.java
+++ src/definition/Login.java
@@ -2,5 +2,21 @@ package definition;
 
 public class Login {
 
+    private String id;
+    private String pswrd;
+    private int count;
+
+    public boolean checkID(String id) {
+        this.id = id;
+        boolean response = false;
+
+        String countstr = Encryption.MD5(id);
+        if (countstr.equals("63a9f0ea7bb98050796b649e85481845")) {
+            this.count = 0;
+            response = true;
+        }
+        return response;
+    }
+
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,24 @@
 package main;
 
+import definition.Login;
+
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Login lgn = new Login();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Login ID");
+        String lgnID = sc.nextLine();
+        System.out.println("Enter Login Password :-");
+        String lgnPSWRD = sc.nextLine();
+
+        if (lgn.checkID(lgnID)) {
+
+        }
+
+    }
+
 }
