2d36e414192f052aa22c6e1c4c3c39d1fc1a4c45
TheEternalShenron <rachit.gla_cs18@gla.ac.in>
2020-02-16 10:18:21 +0530

    PhoneNumberADT completed

--- src/adt/PhoneNumberADT.java
+++ src/adt/PhoneNumberADT.java
@@ -1,4 +1,14 @@
 package adt;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public interface PhoneNumberADT {
+
+    String addIntoDB(String name) throws SQLException;
+
+    void delFromDB(String name);
+
+    ArrayList getFromDB();
+
 }
